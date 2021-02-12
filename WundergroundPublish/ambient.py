bde55e972a63eca95ebe2198374632c7c9a4d2a1
James Wilson <james@drakeapps.com>
2021-02-12 09:01:07 -0600

    Possibly working ambient socket
    This connected and worked once, and now refuses to connect again.
    
    I _think_ this is some ambient api issue, but the node module continues to work for me

--- WundergroundPublish/ambient.py
+++ WundergroundPublish/ambient.py
@@ -18,22 +18,25 @@ class Stream:
 
 		self.verbose = verbose
 
-		self.sio = socketio.AsyncClient()
-
-		self.sio.on("subscribed", self.handle_message)
-
-		self.sio.on("data", self.handle_message)
+		self.sio = socketio.AsyncClient(logger=True, engineio_logger=True)
 
 	async def handle_message(self, message):
 		if self.verbose:
 			print(f"ambient new message: {message}")
-		await self.callback(message)
+		# right now, we're assuming one device connected
+		# TODO: not that
+		device_info = message[0]
+		await self.callback(device_info)
+	
+	async def disconnect(self, message):
+		if self.verbose:
+			print(f"ambient: disconnected")
 
 	async def run_loop(self, callback):
 
 		self.callback = callback
 
-		url = f"ws://rt.ambientweather.net/?api=1&applicationKey={self.application_key}"
+		url = f"https://rt.ambientweather.net/?api=1&applicationKey={self.application_key}"
 		if self.verbose:
 			print(f"ambient: connecting to {url}")
 				
@@ -41,6 +44,12 @@ class Stream:
 
 		if self.verbose:
 			print(f"ambient: connected, sending init")
+		
+		self.sio.on("subscribed", self.handle_message)
+
+		self.sio.on("data", self.handle_message)
+
+		# self.sio.on("disconnect", self.disconnect)
 
 		await self.sio.emit('subscribe', {
 			'apiKeys': [
@@ -50,69 +59,7 @@ class Stream:
 
 		if self.verbose:
 			print(f"ambient: stream initialized")
-
+		
 		await self.sio.wait()
-		
+		print(f"ambient: sio wait finished")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from aiohttp import ClientSession
-# from aioambient import Client
-
-
-# class Stream:
-# 	def __init__(self, api_key, application_key, device=None, verbose=False):
-# 		self.api_key = api_key
-# 		self.application_key = application_key
-
-# 		self.verbose = verbose
-
-		
-	
-# 	async def run_loop(self, callback):
-# 		self.callback = callback
-# 		if self.verbose:
-# 			print(f"Connecting to Ambient weather API with API key {self.api_key} Application key {self.application_key}")
-		
-# 		self.client = Client(self.api_key, self.application_key)
-# 		self.client.websocket.async_on_subscribed(self.parse_data)
-# 		self.client.websocket.async_on_data(self.parse_data)
-# 		self.client.websocket.on_connect(self.connect_message)
-# 		self.client.websocket.on_disconnect(self.disconnect_message)
-
-# 		print("starting ambient connection")
-
-# 		await self.client.websocket.connect()
-
-# 		print("ambient connection gone, disconnecting")
-		
-# 		await self.client.websocket.disconnect()
-
-# 	def connect_message(self):
-# 		if self.verbose:
-# 			print(f"Connected to Ambient weather API")
-	
-# 	def disconnect_message(self):
-# 		if self.verbose:
-# 			print(f"Disconnected from Ambient weather API")
-		
-# 	async def parse_data(self, data):
-# 		print(data)
-# 		await self.callback(data)
-	
